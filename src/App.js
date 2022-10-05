fae93c2968b817910da64adcb436897437ee9554
Juan Pablo Melo <melo.juanp@gmail.com>
2022-10-05 15:46:09 -0300

    se cambio .map a Clima

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import datos from './data';
 import Clima from './components/Clima';
 import Navbar from './components/Navbar';
 
@@ -7,16 +6,9 @@ function App() {
   return (
     <div>
       <Navbar title="My Weather" />
-      <div className='container d-flex'>
-        {
-          datos.map(dato=>
-            <Clima
-              key={dato.city}
-              city={dato.city}
-              lat={dato.lat}
-              lon={dato.lon}
-            />
-        )}
+      <div>
+        <Clima />
+
       </div>
       
     </div>
--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -1,33 +1,42 @@
 import React from 'react'
 import Api from './Api'
+import datos from './data';
 import { IoIosCloseCircle } from "react-icons/io";
 
-function Clima({ city, lat, lon }) {
+function Clima() {
   return (
-    <div className='card m-5' style={{width: "18rem", height:"22rem"}}>
-        <div className='card-header text-end'>
-            <button className='btn' type="submit">
-            <IoIosCloseCircle style={{width: "2rem", height:"2rem"}}></IoIosCloseCircle>
-            </button>
-        </div>
-         
-        {/* <hr /> */}
-        <div className='card-body'>
-            <h3 className='card-title text-center' style={{height:"5rem"}}>{city}</h3>
-            <div className='text-muted'>
-                <h5>Latitud: {lat}</h5>
-                <h5>Longitud: {lon}</h5>
-                <Api 
-                    lat={lat}
-                    lon={lon}
-                />
-            </div>
-            <div className='row text-center'>
-                <div className='col'>Temp.</div>
-                <div className='col'>Vel. Viento</div>
-            </div>
-        </div>
-        
+      <div className='container d-flex'>
+        {
+            datos.map(({city, lat, lon, index}) => {
+                return(
+                    <div key={index} className='card m-5' style={{width: "18rem", height:"22rem"}}>
+                        <div className='card-header text-end'>
+                            <button className='btn' type="submit">
+                                <IoIosCloseCircle style={{width: "2rem", height:"2rem"}}></IoIosCloseCircle>
+                            </button>
+                        </div>
+                        <div className='card-body'>
+                            <div>
+                                <h3 className='card-title text-center' style={{height:"5rem"}}>{city}</h3>
+                                <div className='text-muted'>
+                                    <h5>Latitud: {lat}</h5>
+                                    <h5>Longitud: {lon}</h5>
+                                    <Api 
+                                        lat={lat}
+                                        lon={lon}
+                                    />
+                                </div>
+                                <div className='row text-center'>
+                                    <div className='col'>Temp.</div>
+                                    <div className='col'>Vel. Viento</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })
+        }
+    
     </div>
   )
 }
